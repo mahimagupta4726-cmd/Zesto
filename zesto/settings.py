29197683502204e1bf4cd31e8287993eca53309a
Mahima Gupta <mahimagupta4726@gmail.com>
2026-07-13 17:34:05 +0530

    Configure Vercel deployement

--- zesto/settings.py
+++ zesto/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zesto-codealpha-2024-mahima-gupta'
 DEBUG = True
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["http://127.0.0.1:8000","localhost",".vercel.app"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -63,7 +63,8 @@ TIME_ZONE = 'Asia/Kolkata'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+STATIC_ROOT=BASE_DIR/"staticfiles"
 STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
